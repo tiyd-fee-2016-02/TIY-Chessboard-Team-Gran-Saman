b082e39e8080120d761fded550b77f35ac91a4ce
Dennis Goldin <dennis.goldin@gmail.com>
2016-03-04 14:30:21 -0500

    Add JavaScript click event listener

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,23 @@
-// scripts.js
+$(function () {
+  'use strict';
+
+var board = [
+  ['R','N','B','Q','K','B','N','R'],
+  ['P','P','P','P','P','P','P','P'],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  ['p','p','p','p','p','p','p','p'],
+  ['r','n','b','q','k','b','n','r'] ];
+
+  console.log(board.join('\n') + '\n\n');
+
+
+  $("[class*='btn-']").on("click", function(e)) {
+    e.preventDefault();
+    console.log("*** button click ***");
+  })
+
+
+});  // End of file.
