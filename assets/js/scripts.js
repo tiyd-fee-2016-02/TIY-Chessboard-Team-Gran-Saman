d44358ecfeb6a2423fbdd8d1f872d9cf1f98b3ef
Dennis Goldin <dennis.goldin@gmail.com>
2016-03-06 01:32:50 -0500

    Add beast mode functionality, bug fixes from testing branch and refactor / clean up code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 $(function () {
   'use strict';
 
-  var entityCodex = {
+  var entityCodes = {
     "R" : "&#9820;",
     "N" : "&#9822;",
     "B" : "&#9821",
@@ -17,8 +17,6 @@ $(function () {
     " " : " "
   };
 
-  console.log(entityCodex["R"]);
-
   var board0 = [
     ['R','N','B','Q','K','B','N','R'],
     ['P','P','P','P','P','P','P','P'],
@@ -28,8 +26,6 @@ $(function () {
     [' ',' ',' ',' ',' ',' ',' ',' '],
     ['p','p','p','p','p','p','p','p'],
     ['r','n','b','q','k','b','n','r'] ];
-
-    // console.log(board0.join('\n') + '\n\n');
 
     var board1 = [
       ['R','N','B','Q','K','B','N','R'],
@@ -41,8 +37,6 @@ $(function () {
       ['p','p','p',' ','p','p','p','p'],
       ['r','n','b','q','k','b','n','r'] ];
 
-      // console.log(board1.join('\n') + '\n\n');
-
     var board2 = [
       ['R','N','B','Q','K','B',' ','R'],
       ['P','P','P','P','P','P','P','P'],
@@ -52,8 +46,6 @@ $(function () {
       [' ',' ',' ',' ',' ',' ',' ',' '],
       ['p','p','p',' ','p','p','p','p'],
       ['r','n','b','q','k','b','n','r'] ];
-
-      // console.log(board2.join('\n') + '\n\n');
 
     var board3 = [
       ['R','N','B','Q','K','B',' ','R'],
@@ -65,8 +57,6 @@ $(function () {
       ['p','p',' ',' ','p','p','p','p'],
       ['r','n','b','q','k','b','n','r'] ];
 
-      // console.log(board3.join('\n') + '\n\n');
-
     var board4 = [
       ['R','N','B','Q','K','B',' ','R'],
       ['P','P','P','P',' ','P','P','P'],
@@ -76,8 +66,6 @@ $(function () {
       [' ',' ',' ',' ',' ',' ',' ',' '],
       ['p','p',' ',' ','p','p','p','p'],
       ['r','n','b','q','k','b','n','r'] ];
-
-      // console.log(board4.join('\n') + '\n\n');
 
     var board5 = [
       ['R','N','B','Q','K','B',' ','R'],
@@ -89,8 +77,6 @@ $(function () {
       ['p','p',' ',' ','p','p',' ','p'],
       ['r','n','b','q','k','b','n','r'] ];
 
-      // console.log(board5.join('\n') + '\n\n');
-
     var board6 = [
       ['R','N','B','Q','K','B',' ','R'],
       ['P','P','P',' ',' ','P','P','P'],
@@ -100,8 +86,6 @@ $(function () {
       [' ',' ',' ',' ',' ',' ','p',' '],
       ['p','p',' ',' ','p','p',' ','p'],
       ['r','n','b','q','k','b','n','r'] ];
-
-      // console.log(board6.join('\n') + '\n\n');
 
     var board7 = [
       ['R','N','B','Q','K','B',' ','R'],
@@ -113,8 +97,6 @@ $(function () {
       ['p','p',' ',' ','p','p','b','p'],
       ['r','n','b','q','k',' ','n','r'] ];
 
-      // console.log(board7.join('\n') + '\n\n');
-
     var board8 = [
       ['R','N','B','Q','K',' ',' ','R'],
       ['P','P','P',' ','B','P','P','P'],
@@ -124,8 +106,6 @@ $(function () {
       [' ',' ',' ',' ',' ',' ','p',' '],
       ['p','p',' ',' ','p','p','b','p'],
       ['r','n','b','q','k',' ','n','r'] ];
-
-      // console.log(board8.join('\n') + '\n\n');
 
     var board9 = [
       ['R','N','B','Q','K',' ',' ','R'],
@@ -137,9 +117,7 @@ $(function () {
       ['p','p',' ',' ','p','p','b','p'],
       ['r','n','b','q','k',' ',' ','r'] ];
 
-      // console.log(board9.join('\n') + '\n\n');
-
-// Testing how to make a deep copy of an object
+// Testing how to make a deep copy of an object - please do not remove
 //
 // var moves = {};
 //
@@ -159,8 +137,6 @@ $(function () {
 // console.log(moves[0].join('\n') + '\n\n');
 // console.log(moves[1].join('\n') + '\n\n');
 
-  var moveCount = 0;
-
   var moves = {
     0 : board0,
     1 : board1,
@@ -174,129 +150,94 @@ $(function () {
     9 : board9
   }
 
-  console.log(moves);
-  $.each(moves, function(index) {
-    console.log(moves[index].join('\n') + '\n\n');
-  });
+  // console.log(moves);
+  // $.each(moves, function(index) {
+  //   console.log(moves[index].join('\n') + '\n\n');
+  // });
 
-var gamePaused = false;
-var timeoutId;
-var delay = 1000;
+  var moveCount = 0;
+  var gamePaused = false;
+  var timeoutId;
+  var delay = 1000;
 
+  // play button and pause button click listerner
   $(".btn-play, .btn-pause").on("click", function(e) {
     e.preventDefault();
-
-
     if ($(this).hasClass("btn-play")) {
-      console.log("*** play button click ***");
+      // console.log("*** play button click ***");
       gamePaused = false;
       $.each(moves, function(index) {
-
-        if (index < moveCount) {
+        if (index < moveCount || gamePaused) {
           return;
         }
-console.log(timeoutId);
         timeoutId = window.setTimeout(function() {
-console.log(timeoutId);
-          if (index >= moveCount && moveCount >= 0 && moveCount < 10) {
-
+          if (index >= moveCount && moveCount >= 0 && moveCount < 9) {
             if (gamePaused) {
-              // window.clearTimeout(timeoutId);
               return;
             }
-
-            render(moves[index]);
             moveCount++;
-            console.log("Index of render() call = > " + index);
+            render(moves[moveCount]);
+            // console.log("Index of render() call = > " + index);
           }
         }, delay);
-        delay += 1000; // creates an 1s interval between interations
-
+        delay += 1000; // creates a 1s interval between interations
       });  //  end of each
       delay = 1000;  // reset
-
     }
-
     if ($(this).hasClass("btn-pause")) {
-      console.log("*** pause button click ***");
-      // window.clearTimeout(timeoutId);
+      // console.log("*** pause button click ***");
       gamePaused = true;
     }
+  }); // end of play and pause button listener
 
-  });
-
-  $(".btn-fast-forward").on("click", function(e) {
+  // fast forward button and rewind button click listener
+  $(".btn-fast-forward, .btn-rewind").on("click", function(e) {
     e.preventDefault();
-    moveCount = 9;
-    render(board9);
-    console.log("*** fast forward button click ***");
-  });
-
-  $(".btn-rewind").on("click", function(e) {
-    e.preventDefault();
-    moveCount = 0;
-    render(board0);
-    console.log("*** rewind button click ***");
-  });
-
-  $("[class*='btn-step-']").on("click", function(e) {
-    e.preventDefault();
-    console.log("*** step button click ***");
-    console.log("moveCount => " + moveCount);
-    if (moveCount >= 0 && moveCount <= 9) {
-
-      if ($(this).hasClass("btn-step-forward") && moveCount >= 0 && moveCount < 9) {
-        moveCount++;
-        console.log("*** step forward button click ***");
-      } else if ($(this).hasClass("btn-step-back") && moveCount > 0 && moveCount <=  9) {
-        moveCount--;
-        console.log("*** step back button click ***");
-      }
-
-      if (moveCount === 0) {
-        render(board0);
-      } else if (moveCount === 1) {
-        render(board1);
-      } else if (moveCount === 2) {
-        render(board2);
-      } else if (moveCount === 3) {
-        render(board3);
-      } else if (moveCount === 4) {
-        render(board4);
-      } else if (moveCount === 5) {
-        render(board5);
-      } else if (moveCount === 6) {
-        render(board6);
-      } else if (moveCount === 7) {
-        render(board7);
-      } else if (moveCount === 8) {
-        render(board8);
-      } else if (moveCount === 9) {
-        render(board9);
-      }
+    if ($(this).hasClass("btn-fast-forward")) {
+      moveCount = 9;
+      // console.log("*** fast forward button click ***");
+    } else if ($(this).hasClass("btn-rewind")) {
+      moveCount = 0;
+      // console.log("*** rewind button click ***");
     }
-
+    render(moves[moveCount]);
   })
 
-  // $("[class*='btn-']").on("click", function(e) {
-  //   e.preventDefault();
-  //   console.log("*** button click ***");
-  //
-  //   if (moveCount >= 0 && moveCount <= 10) {
-  //     if (moveCount == ) {
-  //
-  //     } else if {}
-  //   }
-  // })
-
-  // draw game board
-  function render(arr) {
-    for(var rowCount = 0; rowCount < 8; rowCount++) {
-      var rowArray = $("[class*='game-row-" + rowCount + "'] div");
-      for(var colCount = 0; colCount < 8; colCount++ ) {
-        $(rowArray[colCount]).html(entityCodex[arr[rowCount][colCount]]);
+  // step forward and step backward button click listener
+  $("[class*='btn-step-']").on("click", function(e) {
+    e.preventDefault();
+    // console.log("*** step button click ***");
+    // console.log("moveCount => " + moveCount);
+    if (moveCount >= 0 && moveCount <= 9) {
+      if ($(this).hasClass("btn-step-forward") && moveCount >= 0 && moveCount < 9) {
+        moveCount++;
+        // console.log("*** step forward button click ***");
+      } else if ($(this).hasClass("btn-step-back") && moveCount > 0 && moveCount <=  9) {
+        moveCount--;
+        // console.log("*** step back button click ***");
       }
+      render(moves[moveCount]);
     }
-  }  // end of render()
+  })
 
-});  // End of file.
+  // draw game board - jQuery version
+  function render(myArray) {
+    $.each(myArray, function (rowCount) {
+      $("[class*='game-row-" + rowCount + "'] div").each( function(colCount) {
+        $(this).html(entityCodes[myArray[rowCount][colCount]]);
+      });
+    });
+  }   // end of render()
+
+  // draw game board - vanilla JavaScript version - please do not remove
+  // function render(arr) {
+  //   for(var rowCount = 0; rowCount < 8; rowCount++) {
+  //     // var rowArray = $("[class*='game-row-" + rowCount + "'] div");
+  //     var rowArray = document.querySelectorAll("[class*='game-row-" + rowCount + "'] div");
+  //     for(var colCount = 0; colCount < 8; colCount++ ) {
+  //       $(rowArray[colCount]).html(entityCodes[arr[rowCount][colCount]]);
+  //     }
+  //   }
+  //}   end of render()
+
+});  // end of JavaScript
